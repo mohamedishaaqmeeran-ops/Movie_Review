545b8cbe8a63785cc21dce43f3b0b57ea156668e
unknown <mohamedishaaqmeeran@gmail.com>
2026-02-28 13:31:01 +0530

    update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,46 +1,75 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/logo.jpg";
 
-const NavBar = () => {
-    return (
-        <div className="flex justify-between items-center p-2 bg-white shadow">
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
 
-            <h1 className="text-md sm:text-2xl font-bold text-pink-600">
-                🎬 MovieReview
-            </h1>
-
-            <ul className="flex gap-2 sm:gap-20">
-
-                <li>
-                    <Link
-                        className="p-2 hover:bg-pink-700 hover:text-white  text-pink-600"
-                        to="/"
-                    >
-                        Home
-                    </Link>
-                </li>
-
-                <li>
-                    <Link
-                        className="p-2 hover:bg-pink-700 hover:text-white  text-pink-600"
-                        to="/register"
-                    >
-                        Register
-                    </Link>
-                </li>
-
-                <li>
-                    <Link
-                        className="p-2 hover:bg-pink-700 hover:text-white text-pink-600"
-                        to="/login"
-                    >
-                        Login
-                    </Link>
-                </li>
-
-            </ul>
-
+  return (
+    <nav className="bg-pink-600 text-white ">
+      <div className="flex justify-between items-center px-4 py-3">
+        <div className="flex items-center gap-2">
+          <img
+            src={logo}
+            alt="logo"
+            className="border-2 h-10 rounded-full border-pink-800"
+          />
+          <h1 className="font-bold text-lg">Movie Review</h1>
         </div>
-    )
-}
+        <button className="md:hidden text-2xl" onClick={() => setMenu(!menu)}>
+          ☰
+        </button>
 
-export default NavBar;
+        <div className="hidden md:flex gap-6 font-semibold">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-white text-pink-600 px-3 py-2 rounded-lg font-semibold text-sm md:text-base"
+                : "px-3 py-2 rounded-lg font-semibold text-sm md:text-base hover:bg-blue-950 hover:text-yellow-400"
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/register"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-white text-pink-600 px-3 py-2 rounded-lg font-semibold text-sm md:text-base"
+                : "px-3 py-2 rounded-lg font-semibold text-sm md:text-base hover:bg-blue-950 hover:text-yellow-400"
+            }
+          >
+            Register
+          </NavLink>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-white text-pink-600 px-3 py-2 rounded-lg font-semibold text-sm md:text-base"
+                : "px-3 py-2 rounded-lg font-semibold text-sm md:text-base hover:bg-blue-950 hover:text-yellow-400"
+            }
+          >
+            Login
+          </NavLink>
+        </div>
+      </div>
+
+      {menu && (
+        <div className="md:hidden flex flex-col gap-3 px-4 pb-4 font-semibold">
+          <Link to="/" onClick={() => setMenu(false)}>
+            Home
+          </Link>
+          <Link to="/register" onClick={() => setMenu(false)}>
+            Register
+          </Link>
+          <Link to="/login" onClick={() => setMenu(false)}>
+            Login
+          </Link>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/Pages/Reviewlist.jsx
+++ src/Pages/Reviewlist.jsx
@@ -2,88 +2,71 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Reviewlist = () => {
+  const [movies, setMovies] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
-    const [movies, setMovies] = useState([]);
-    const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://699e877d78dda56d396a7fac.mockapi.io/movie")
+      .then((res) => {
+        setMovies(res.data);
+      });
+    axios
+      .get("https://699e877d78dda56d396a7fac.mockapi.io/review")
+      .then((res) => {
+        setReviews(res.data);
+      });
+  }, []);
 
-    useEffect(() => {
+  return (
+    <div className="p-1 md:p-5 pr-0  md:w-100 bg-pink-600 min-h-screen">
+      <h2 className="text-xl mb-4 font-bold text-white text-center">
+        All Movie Reviews 🎬
+      </h2>
 
-        axios.get(
-            "https://699e877d78dda56d396a7fac.mockapi.io/movie"
-        )
-            .then(res => {
-                setMovies(res.data);
-            });
-        axios.get(
-            "https://699e877d78dda56d396a7fac.mockapi.io/review"
-        )
-            .then(res => {
-                setReviews(res.data);
-            });
+      {movies.map((movie) => {
+        const movieReviews = reviews.filter(
+          (r) => String(r.movieId) === String(movie.id),
+        );
+        const avgRating =
+          movieReviews.length > 0
+            ? (
+                movieReviews.reduce((sum, r) => sum + Number(r.rating), 0) /
+                movieReviews.length
+              ).toFixed(1)
+            : 0;
+        return (
+          <div
+            key={movie.id}
+            className="mb-4 border p-2 mr-1 bg-white rounded-lg"
+          >
+            <h3 className="font-bold text-lg text-pink-500">
+              Movie Name : {movie.name}
+              <hr></hr>
+            </h3>
+            <p className="text-pink-500 font-semibold">
+              ⭐ Average Rating : {avgRating} / 5
+            </p>
 
-    }, []);
-
-
-    return (
-
-        <div className="p-1 md:p-5 pr-0  md:w-100 bg-pink-600 min-h-screen">
-
-            <h2 className="text-xl mb-4 font-bold text-white text-center">
-                All Movie Reviews 🎬
-            </h2>
-
-
-            {
-                movies.map(movie => {
-
-                    const movieReviews =
-                        reviews.filter(
-                            r => String(r.movieId) === String(movie.id)
-                        );
-
-                    return (
-
-                        <div key={movie.id}
-                            className="mb-4 border p-2 mr-1 bg-white rounded-lg">
-
-                            <h3 className="font-bold text-lg text-pink-500">
-                                Movie Name : {movie.name}<hr></hr>
-                            </h3>
-
-
-                            {
-                                movieReviews.length === 0 ?
-
-                                    <p className="text-pink-950">
-                                        No Reviews
-                                    </p>
-
-                                    :
-
-                                    movieReviews.map(r => (
-                                        <p className="text-pink-950" key={r.id}>
-                                            username : {r.name}<br></br>
-                                            Rating : {r.rating}<br></br>
-                                            Review : {r.review}<hr></hr>
-                                        </p>
-
-                                    ))
-
-                            }
-
-                        </div>
-
-                    )
-
-                })
-
-            }
-
-
-        </div>
-
-    )
-
-}
+            {movieReviews.length === 0 ? (
+              <p className="text-pink-950">No Reviews</p>
+            ) : (
+              movieReviews.map((r) => (
+                <p className="text-pink-950" key={r.id}>
+                  username : {r.name}
+                  <br></br>
+                  Rating : {r.rating}
+                  <br></br>
+                  Review : {r.review}
+                  <hr></hr>
+                </p>
+              ))
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Reviewlist;
